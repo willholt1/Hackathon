619c61f23bcb0f61a9c99d46137758b4dd1c60a0
willholt1 <56679308+willholt1@users.noreply.github.com>
2020-03-31 21:14:06 +0100

    Initial commit

---
+++ app/src/main/java/mmu/ac/uk/hackathon/MainActivity.java
@@ -0,0 +1,14 @@
+package mmu.ac.uk.hackathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
